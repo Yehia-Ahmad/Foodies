ca6e4bfb2c0e3153d8265a1788e06dc6fdb0250c
Yehia <Yehia>
2024-07-22 00:46:59 +0300

    handle meals page

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
     return (
-        <div>
-            <h1>Meals</h1>
-            <p><Link href="/meals/share">Share</Link></p>
-            <p><Link href="/meals/Meal-1">Meal-1</Link></p>
-            <p><Link href="/meals/Meal-2">Meal-2</Link></p>
-        </div>
+        <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious meals, Created{' '}
+                    <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={styles.cta}>
+                    <Link href="/meals/share">
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+
+            <main className={styles.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     );
 }
