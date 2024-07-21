4437880368b248213be92c2247e0aeadfc229a72
Yehia <Yehia>
2024-07-22 01:11:20 +0300

    Add loading content

---
+++ app/meals/loading-out.tsx
@@ -0,0 +1,7 @@
+import styles from './loading.module.css'
+
+export default function MealsLoadingPage() {
+    return (
+        <div className={styles.loading}>Fatching Meals...</div>
+    );
+}
--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import getMeals from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
     const meals = await getMeals();
+    return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
     return (
         <>
             <header className={styles.header}>
@@ -24,7 +28,9 @@ export default async function MealsPage() {
             </header>
 
             <main className={styles.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<div className={styles.loading}>Fatching Meals...</div>}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
